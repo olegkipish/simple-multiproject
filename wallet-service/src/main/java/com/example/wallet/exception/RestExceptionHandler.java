c5819a6e7a6536594d8c02c682748765be483532
Мартиненко Олег Олегович <martinenko@wideup.org>
2017-11-07 23:03:29 +0200

    Add rest exception handlers

---
+++ wallet-service/src/main/java/com/example/wallet/exception/RestExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.wallet.exception;
+
+import com.example.wallet.api.classes.GeneralResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<GeneralResponse> exceptionHandler(Exception ex) {
+		GeneralResponse error = new GeneralResponse();
+		error.setErrorCode(HttpStatus.BAD_REQUEST.value());
+		error.setErrorDescription("The request could not be understood by the server due to malformed syntax.");
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+
+}
